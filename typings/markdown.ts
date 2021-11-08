237907cdd078aa55f8611cd9bacffc9ac79dc53e
eggsnham07 <eggsnham07@gmail.com>
2021-11-08 10:38:33 -0700

    Quick styles.css update

--- typings/markdown.ts
+++ typings/markdown.ts
@@ -1,3 +1,20 @@
+type VideoData = {
+    author_name: string,
+    author_url: string,
+    height: number,
+    html: string,
+    provider_name: string,
+    provider_url: string,
+    thumbnail_height: number,
+    thumbnail_url: string,
+    thumbnail_width: number,
+    title: string,
+    type: string,
+    url: string,
+    verion: string,
+    width: string
+}
+
 const h1 = /^#\s(.*$)/gm
 const h2 = /^##\s(.*$)/gm
 const h3 = /^###\s(.*$)/gm
@@ -17,11 +34,43 @@ const ib = /\*\*\*(.*?)\*\*\*/gm
 const li = /- (.*$)/gm
 const dli = /- - (.*$)/gm
 
+const youtube = /https:\/\/www.youtube.com\/watch\?v=(.*$)/gm
+
 /**
  * 
  * @param {String} str 
  */
 export function parse(str:string): string {
+    const style = {
+        iframe: `border-radius: 20px;
+        margin: 0 auto;
+        width: 100%;
+        height: 250px;`,
+        card: `font-family: Arial, Helvetica, sans-serif;
+        border: 3px solid #333;
+        border-radius: 20px;
+        min-height: 300px;
+        width: 25%;
+        margin: 0 auto;
+        padding: 12px;`
+    };
+    var data:VideoData;
+    //@ts-ignore
+    if (str.includes("https://www.youtube.com") && typeof $ !== "undefined") {
+        var url = str.match(youtube);
+        //@ts-ignore
+        if ((url === null || url === void 0 ? void 0 : url.length) > 0) {
+            //@ts-ignore
+            $.getJSON("https://noembed.com/embed", {
+                format: "json",
+                //@ts-ignore
+                url: url[0]
+            }, function (d:VideoData) {
+                data = d;
+                console.log("Working...")
+            });
+        }
+    }
     return str
         .replace(h1, "<h1 id=\'$1\'>$1</h1><hr>")
         .replace(h2, "<h2 id=\'$1\'>$1</h2>")
